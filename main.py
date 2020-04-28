5acc6f9e8aa60d409b89b034682b3d8413460b73
Jake Nieto <jnieto@Jakes-MBP.fios-router.home>
2020-04-28 14:54:19 -0400

    c

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ COUNTY_COL = 1
 STATE_COL= 2
 header = [
             "FIPS","Admin2","Province_State","Country_Region",
-            "Last_Update","Lat,Long_","Confirmed","Deaths","Recovered","Active"
+            "Last_Update","Lat,Long_","Confirmed","Deaths","Recovered","Active",
          ]
 
 ''' This function takes in the number of days from the current
